30c38d3d2d729c815d1255b3a4883dbea54a9f13
raybogman <bogman@bogman.info>
2021-05-15 17:42:15 +0200

    serverless 1.0.1 - pub issue fix

--- serverless.sh
+++ serverless.sh
@@ -5,25 +5,25 @@
 
 dist='./dist'
 base='./serverless'
-public='./serverless/public'
-public_test='./serverless/public_test'
+pub_prod='./serverless/pub_prod'
+pub_test='./serverless/pub_test'
 CONFIG_ENV_FILE='./.env'
 date=$(date '+%Y-%m-%d--%H:%M:%S')
 
-# Copy serverless files to 'public' folder
+# Copy serverless files to 'pub' folder
 # Copy the MAGENTO_BACKEND_URL into the server.js
-# Rsync /dist to /public folder including additional files 
+# Rsync /dist to /pub prod or test folder including additional files 
 
 if [ "$1" == test ]; then
-    find $base/ \( -name '*.json' -o -name '*.js' -o -name 'Dockerfile' \) | while read files ; do cp -Rp $files $public_test/; done
+    find $base/ \( -name '*.json' -o -name '*.js' -o -name 'Dockerfile' \) | while read files ; do cp -Rp $files $pub_test/; done
     url=$(awk -F '"' '/MAGENTO_BACKEND_URL=/ {print $1}' $CONFIG_ENV_FILE | awk '{print substr($0, 21, length($0) - 0)}')
     sed -i '' -e "s|example.com|$url|g" $public_test/server.js
-    rsync -r $dist/* $public_test
+    rsync -r $dist/* $pub_test
 else
-    find $base/ \( -name '*.json' -o -name '*.js' -o -name 'Dockerfile' \) | while read files ; do cp -Rp $files $public/; done
+    find $base/ \( -name '*.json' -o -name '*.js' -o -name 'Dockerfile' \) | while read files ; do cp -Rp $files $pub_prod/; done
     url=$(awk -F '"' '/MAGENTO_BACKEND_URL=/ {print $1}' $CONFIG_ENV_FILE | awk '{print substr($0, 21, length($0) - 0)}')
-    sed -i '' -e "s|example.com|$url|g" $public/server.js
-    rsync -r $dist/* $public
+    sed -i '' -e "s|example.com|$url|g" $pub_prod/server.js
+    rsync -r $dist/* $pub_prod
 fi
 
 # Optional auto git push option && Alternative could be 'copilot svc deploy'
