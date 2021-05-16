c21c516b545ada9773e6989d6b15a8193e700475
raybogman <bogman@bogman.info>
2021-05-16 12:42:40 +0200

    serverless 1.0.6 - fix install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -u
 
 git clone git@github.com:magento/pwa-studio.git
+cd pwa-studio
 git clone git@github.com:raybogman/serverless.git
 npm i climod-add-script
 yarn climod-add-script --name 'build:serverless' --cmd 'yarn run build:prod && ./serverless/serverless.sh'
