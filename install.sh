76f209f2123bf3c34616344b2f68a76e86c5d27c
raybogman <bogman@bogman.info>
2021-05-16 12:22:43 +0200

    serverless 1.0.4 - update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -u
-
-git init 
+ 
+git clone git@github.com:raybogman/serverless.git
+git init
 npm i climod-add-script
 yarn climod-add-script --name 'build:serverless' --cmd 'yarn run build:prod && ./serverless/serverless.sh'
 yarn climod-add-script --name 'build:serverless:test' --cmd 'yarn run build:prod && ./serverless/serverless.sh test'
