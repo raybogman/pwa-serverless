16bcaa8ac37050c3e458caab25ce04302ad6cd07
raybogman <bogman@bogman.info>
2021-05-16 12:48:04 +0200

    serverless 1.0.9 - test install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ cd pwa-studio
 npm i climod-add-script
 yarn climod-add-script --name 'build:serverless' --cmd 'yarn run build:prod && ./serverless/serverless.sh'
 yarn climod-add-script --name 'build:serverless:test' --cmd 'yarn run build:prod && ./serverless/serverless.sh test'
-git clone git@github.com:raybogman/serverless.git
-cd serverless
-git init
+#git clone git@github.com:raybogman/serverless.git
+#cd serverless
+#git init
