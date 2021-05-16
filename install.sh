1f924f42b5eca48a4c918288d629c7bb6517c229
raybogman <bogman@bogman.info>
2021-05-16 16:49:57 +0200

    serverless 1.0.13 - update install.sh

--- install.sh
+++ install.sh
@@ -8,18 +8,19 @@ climod-add-script --name 'build:serverless' --cmd 'yarn run build:prod && ./serv
 climod-add-script --name 'build:serverless:test' --cmd 'yarn run build:prod && ./serverless/serverless.sh test'
 git clone git@github.com:raybogman/serverless.git
 cd serverless
+rm -rf .git
 git init
 
-echo # Congratulations, you just installed PWA Studio Serverless!!!
+echo "Congratulations, you just installed PWA Studio Serverless                                                     "
 echo 
-echo ##########################################################################################################
-echo #   Next step:
-echo #   - Install AWS CLI  
-echo #     curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-echo #     sudo installer -pkg AWSCLIV2.pkg -target /
-echo #
-echo #   - Install AWS Copilot
-echo #     brew install aws/tap/copilot-cli
-echo ##########################################################################################################
-echo
-echo # How to setup AWS Fargate - https://github.com/raybogman/pwa-studio-serverless 
+echo "##########################################################################################################    "
+echo "#   Next step:                                                                                                "
+echo "#   - Install AWS CLI                                                                                         "
+echo "#     curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"                                      "
+echo "#     sudo installer -pkg AWSCLIV2.pkg -target /                                                              "
+echo "#                                                                                                             "
+echo "#   - Install AWS Copilot                                                                                     "
+echo "#     brew install aws/tap/copilot-cli                                                                        "
+echo "##########################################################################################################    "
+echo 
+echo "# How to setup AWS Fargate - https://github.com/raybogman/pwa-studio-serverless                               "
