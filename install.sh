52fd0857e39d413261facfd7c05a884a4ddfcf87
raybogman <bogman@bogman.info>
2021-05-16 13:01:28 +0200

    serverless 1.0.10 - test install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -u
 
 git clone git@github.com:magento/pwa-studio.git
 cd pwa-studio
-npm i climod-add-script
+yarn global add climod-add-script
 yarn climod-add-script --name 'build:serverless' --cmd 'yarn run build:prod && ./serverless/serverless.sh'
 yarn climod-add-script --name 'build:serverless:test' --cmd 'yarn run build:prod && ./serverless/serverless.sh test'
 #git clone git@github.com:raybogman/serverless.git
